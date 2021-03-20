8832967d2c4f0286cccb06f0704fa52f6619d456
Iwan-G <74765961+Iwan-G@users.noreply.github.com>
2021-03-20 12:13:32 +0800

    able to edit and add new product

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import path from 'path';
 import productRouter from './router/productRouter.js';
 import userRouter from './router/userRouter.js';
 import orderRouter from './router/orderRouter.js';
+import uploadRouter from './router/uploadRouter.js';
 
 dotenv.config();
 
@@ -17,13 +19,15 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/Fashion-file', 
   useCreateIndex: true,
 });
 
-
+app.use('/api/uploads', uploadRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productRouter);
 app.use('/api/orders', orderRouter);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
 });
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
